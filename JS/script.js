eed46f094a9a07f69d7a878583670fac26285d60
eunjeong <ungdunge2@gmail.com>
2021-12-10 13:09:57 -0800

    if user input their information in the form and click submit, the object is logged to the console.

--- JS/script.js
+++ JS/script.js
@@ -12,3 +12,20 @@ darkmode.addEventListener('change', function() {
         body.classList.add('light-theme')
     }
 });
+
+document.querySelector('#frm-btn').addEventListener('click', createform)
+
+function createform(evt) {
+    const form=document.querySelector('form')
+    const formmessage = form[0].value
+    const formname = form[1].value
+    const formemail = form[2].value
+
+    var userinput = {
+        name: formname,
+        message: formmessage,
+        email: formemail
+    }
+
+    console.log(userinput)
+}
